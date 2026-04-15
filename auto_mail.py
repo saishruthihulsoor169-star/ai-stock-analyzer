0ceb5f7bcd1bfb0632b1d3c09b74731545ad5c84
SAI SHRUTHI <saishruthihulsoor169@gmail.com>
2026-04-15 20:16:54 +0530

    Update auto_mail.py

--- auto_mail.py
+++ auto_mail.py
@@ -2,11 +2,12 @@ import smtplib
 from email.mime.text import MIMEText
 import os
 
-def send_email(to_email, subject, body):
-    sender = os.getenv("EMAIL_USER")
+def send_email(to_email, subject, content):
+
+    sender = os.getenv("EMAIL_ID")
     password = os.getenv("EMAIL_PASS")
 
-    msg = MIMEText(body)
+    msg = MIMEText(content)
     msg["Subject"] = subject
     msg["From"] = sender
     msg["To"] = to_email
@@ -15,10 +16,12 @@ def send_email(to_email, subject, body):
         server = smtplib.SMTP("smtp.gmail.com", 587)
         server.starttls()
         server.login(sender, password)
+
         server.sendmail(sender, to_email, msg.as_string())
         server.quit()
-        print("Email sent")
+
+        print("Email sent successfully")
 
     except Exception as e:
-        print("Email error:", e)
+        print("Email failed:", e)
 
